b3de5f0074d9802fdf188a426aa6078435b421dc
tigerccx <tigerccx@sina.cn>
2022-08-20 21:45:16 +0100

    Add LIWList.

--- LIWEngine/Core/Containers/LIWList.h
+++ LIWEngine/Core/Containers/LIWList.h
@@ -7,15 +7,16 @@
 //TODO: Maybe a preallocated pool is better for this. 
 
 template<class T, LIWMemAllocation AllocType = LIWMem_Default>
-class LIWFowardList {
+class LIWList {
 public:
 	struct Element;
 public:
-	typedef LIWFowardList<T, AllocType> this_type;
+	typedef LIWList<T, AllocType> this_type;
 	typedef LIWPointer<Element, AllocType> element_ptr_type;
 private:
 	struct Element {
 		typename element_ptr_type m_next{ liw_c_nullhdl };
+		typename element_ptr_type m_prev{ liw_c_nullhdl };
 		T m_val;
 	};
 public:
@@ -42,7 +43,6 @@ public:
 		//
 		// Step
 		//
-		// Step ops: --itr, itr-- is not allowed for ForwardList iterator. 
 		inline CIterator& operator++() {
 			assert(!m_ptrCur.is_null());
 			m_ptrCur = m_ptrCur->m_next;
@@ -52,6 +52,17 @@ public:
 			assert(!m_ptrCur.is_null());
 			CIterator tmp{ *this };
 			m_ptrCur = m_ptrCur->m_next;
+			return tmp;
+		}
+		inline CIterator& operator--() {
+			assert(!m_ptrCur.is_null());
+			m_ptrCur = m_ptrCur->m_prev;
+			return *this;
+		}
+		inline CIterator operator--(int) {
+			assert(!m_ptrCur.is_null());
+			CIterator tmp{ *this };
+			m_ptrCur = m_ptrCur->m_prev;
 			return tmp;
 		}
 
@@ -100,37 +111,42 @@ public:
 		}
 	};
 public:
-	LIWFowardList() : m_size(0) {
+	LIWList() : m_size(0) {
 		m_prehead = liw_new<AllocType, Element>(
-			Element{ liw_c_nullhdl, T{} }
+			Element{ liw_c_nullhdl, liw_c_nullhdl, T{} }
 		);
+		m_prehead->m_prev = m_prehead->m_next = m_prehead;
 	}
 	template<class ... Args>
-	LIWFowardList(size_t size, Args&&... args) : m_size(size) {
+	LIWList(size_t size, Args&&... args) : m_size(size) {
 		assert(size == 0); // What r u trying to do???
 		m_prehead = liw_new<AllocType, Element>(
-			Element{ liw_c_nullhdl, std::forward<Args>(args)... }
+			Element{ liw_c_nullhdl, liw_c_nullhdl, std::forward<Args>(args)... }
 		);
 		element_ptr_type ptrPrev = m_prehead;
 		for (size_t i = 0; i < size; i++) {
 			auto ptr = liw_new<AllocType, Element>(
-				Element{ liw_c_nullhdl, std::forward<Args>(args)... }
+				Element{ liw_c_nullhdl, liw_c_nullhdl, std::forward<Args>(args)... }
 			);
 			ptrPrev->m_next = ptr;
+			ptr->m_prev = ptrPrev;
 			ptrPrev = ptr;
 		}
+		ptrPrev->m_next = m_prehead;
+		m_prehead->m_prev = ptrPrev;
 	}
-	LIWFowardList(const LIWFowardList& other) {
+	LIWList(const LIWList& other) {
 		m_prehead = liw_new<AllocType, Element>(
-			Element{ liw_c_nullhdl, T{} }
+			Element{ liw_c_nullhdl, liw_c_nullhdl, T{} }
 		);
+		m_prehead->m_prev = m_prehead->m_next = m_prehead;
 		*this = other;
 	}
-	LIWFowardList(LIWFowardList&& other) {
-		*this = std::forward<LIWFowardList>(other);
+	LIWList(LIWList&& other) {
+		*this = std::forward<LIWList>(other);
 	}
-	LIWFowardList& operator=(const LIWFowardList& other) {
-		if (!m_prehead->m_next.is_null()) { //Clean out old data
+	LIWList& operator=(const LIWList& other) {
+		if (m_prehead->m_next != m_prehead) { //Clean out old data
 			destroy_elements();
 		}
 		m_size = other.m_size;
@@ -138,17 +154,20 @@ public:
 		element_ptr_type ptrPrev = m_prehead;
 		for (size_t i = 0; i < m_size; i++) {
 			auto ptr = liw_new<AllocType, Element>(
-				Element{ liw_c_nullhdl, *ptrOther }
+				Element{ liw_c_nullhdl, liw_c_nullhdl, *ptrOther }
 			); 
 			ptrPrev->m_next = ptr;
+			ptr->m_prev = ptrPrev;
 			ptrPrev = ptr;
 			ptrOther = ptrOther->m_next;
 		}
+		ptrPrev->m_next = m_prehead;
+		m_prehead->m_prev = ptrPrev;
 		return *this;
 	}
-	LIWFowardList& operator=(LIWFowardList&& other) {
+	LIWList& operator=(LIWList&& other) {
 		if (!m_prehead.is_null()) {
-			if (!m_prehead->m_next.is_null()) { //Clean out old data
+			if (!m_prehead->m_next != m_prehead) { //Clean out old data
 				destroy_elements();
 			}
 			liw_delete<AllocType>(m_prehead);
@@ -158,7 +177,7 @@ public:
 		return *this;
 	}
 
-	~LIWFowardList() {
+	~LIWList() {
 		if (!m_prehead->m_next.is_null()) {
 			destroy_elements();
 		}
@@ -168,7 +187,7 @@ public:
 	//
 	// Compare
 	//
-	inline bool operator==(const LIWFowardList& other) const {
+	inline bool operator==(const LIWList& other) const {
 		if (m_prehead == other.m_prehead) {
 			return true;
 		}
@@ -186,7 +205,7 @@ public:
 		}
 		return true;
 	}
-	inline bool operator!=(const LIWFowardList& other) const {
+	inline bool operator!=(const LIWList& other) const {
 		return !(*this == other);
 	}
 
@@ -212,10 +231,17 @@ public:
 	inline T get_head_val() const {
 		return m_prehead->m_next->m_val;
 	}
+	inline element_ptr_type get_tail() const {
+		return m_prehead->m_prev;
+	}
+	inline T get_tail_val() const {
+		return m_prehead->m_prev->m_val;
+	}
 
 	//
 	// Iterator
 	//
+	//NOTE: for List, prebeg == end
 	inline Iterator get_prebeg() {
 		return Iterator(m_prehead);
 	}
@@ -223,7 +249,7 @@ public:
 		return Iterator(m_prehead->m_next);
 	}
 	inline Iterator get_end() {
-		return Iterator(element_ptr_type{ liw_c_nullhdl });
+		return get_prebeg();
 	}
 	inline Iterator get_cprebeg() {
 		return CIterator(m_prehead);
@@ -232,56 +258,181 @@ public:
 		return CIterator(m_prehead->m_next);
 	}
 	inline CIterator get_cend() const {
-		return CIterator(element_ptr_type{ liw_c_nullhdl });
+		return get_cprebeg();
 	}
 
 	//
 	// Modifer
 	//
-	inline void push_front(const T& val) {
+	void push_front(const T& val) {
 		auto ptr = liw_new<AllocType, Element>(
-			Element{ liw_c_nullhdl, std::forward<const T>(val) }
+			Element{ liw_c_nullhdl, liw_c_nullhdl, std::forward<const T>(val) }
 		);
-		ptr->m_next = m_prehead->m_next;
+		auto ptrNext = m_prehead->m_next;
+
+		ptr->m_next = ptrNext;
+		ptrNext->m_prev = ptr;
 		m_prehead->m_next = ptr;
+		ptr->m_prev = m_prehead;
+
 		m_size++;
 	}
-	inline void push_front(T&& val) {
+	void push_front(T&& val) {
 		auto ptr = liw_new<AllocType, Element>(
-			Element{ liw_c_nullhdl, std::forward<T>(val) }
+			Element{ liw_c_nullhdl, liw_c_nullhdl, std::forward<T>(val) }
 		);
-		ptr->m_next = m_prehead->m_next;
+		auto ptrNext = m_prehead->m_next;
+
+		ptr->m_next = ptrNext;
+		ptrNext->m_prev = ptr;
 		m_prehead->m_next = ptr;
+		ptr->m_prev = m_prehead;
+
 		m_size++;
 	}
 
-	inline void pop_front() {
+	void pop_front() {
 		assert(m_size != 0); // Cannot pop from an empty List. 
 		auto ptr = m_prehead->m_next;
-		m_prehead->m_next = ptr->m_next;
+		auto ptrNext = ptr->m_next;
+
+		ptrNext->m_prev = m_prehead;
+		m_prehead->m_next = ptrNext;
+
 		liw_delete(ptr);
 		m_size--;
+	}
+
+	void push_back(const T& val) {
+		auto ptr = liw_new<AllocType, Element>(
+			Element{ liw_c_nullhdl, liw_c_nullhdl, std::forward<const T>(val) }
+		);
+		auto ptrPrev = m_prehead->m_prev;
+
+		ptr->m_next = m_prehead;
+		m_prehead->m_prev = ptr;
+		ptrPrev->m_next = ptr;
+		ptr->m_prev = ptrPrev;
+
+		m_size++;
+	}
+	void push_back(T&& val) {
+		auto ptr = liw_new<AllocType, Element>(
+			Element{ liw_c_nullhdl, liw_c_nullhdl, std::forward<T>(val) }
+		);
+		auto ptrPrev = m_prehead->m_prev;
+
+		ptr->m_next = m_prehead;
+		m_prehead->m_prev = ptr;
+		ptrPrev->m_next = ptr;
+		ptr->m_prev = ptrPrev;
+
+		m_size++;
+	}
+
+	void pop_back() {
+		assert(m_size != 0); // Cannot pop from an empty List. 
+		auto ptr = m_prehead->m_prev;
+		auto ptrPrev = ptr->m_prev;
+
+		ptrPrev->m_next = m_prehead;
+		m_prehead->m_prev = ptrPrev;
+
+		liw_delete(ptr);
+		m_size--;
+	}
+
+	Iterator insert(Iterator pos, const T& val) {
+		// Skipping check for if pos is in this container...
+		assert(!pos.m_ptrCur.is_null()); // pos must be valid
+		assert(pos.m_ptrCur != m_prehead); // pos must be valid
+
+		auto ptr = liw_new<AllocType, Element>(
+			Element{ liw_c_nullhdl, liw_c_nullhdl, std::forward<const T>(val) }
+		);
+		auto ptrPrev = pos.m_ptrCur->m_prev;
+
+		ptr->m_next = pos.m_ptrCur;
+		pos.m_ptrCur->m_prev = ptr;
+		ptrPrev->m_next = ptr;
+		ptr->m_prev = ptrPrev;
+
+		m_size++;
+		return Iterator(ptr);
+	}
+	Iterator insert(Iterator pos, T&& val) {
+		// Skipping check for if pos is in this container...
+		assert(!pos.m_ptrCur.is_null()); // pos must be valid
+		assert(pos.m_ptrCur != m_prehead); // pos must be valid
+
+		auto ptr = liw_new<AllocType, Element>(
+			Element{ liw_c_nullhdl, liw_c_nullhdl, std::forward<T>(val) }
+		);
+		auto ptrPrev = pos.m_ptrCur->m_prev;
+
+		ptr->m_next = pos.m_ptrCur;
+		pos.m_ptrCur->m_prev = ptr;
+		ptrPrev->m_next = ptr;
+		ptr->m_prev = ptrPrev;
+
+		m_size++;
+		return Iterator(ptr);
+	}
+	template<class Iterator1>
+	Iterator insert(Iterator pos, Iterator1 beg, Iterator1 end) {
+		assert(!pos.m_ptrCur.is_null()); // pos must be valid
+		assert(pos.m_ptrCur != m_prehead); // pos must be valid
+
+		auto ptrNext = pos.m_ptrCur;
+		auto ptrPrev = pos.m_ptrCur->m_prev;
+		auto ptrPrevTmp = ptrPrev;
+		
+		for (auto itr = beg; itr != end; ++itr) {
+			auto ptr = liw_new<AllocType, Element>(
+				Element{ liw_c_nullhdl, liw_c_nullhdl, *itr }
+			);
+			ptrPrev->m_next = ptr;
+			ptr->m_prev = ptrPrev;
+			ptrPrev = ptr;
+			m_size++;
+		}
+		ptrPrev->m_next = ptrNext;
+		ptrNext->m_prev = ptrPrev;
+		return Iterator(ptrPrevTmp->m_next);
 	}
 
 	Iterator insert_after(Iterator pos, const T& val) {
 		// Skipping check for if pos is in this container...
 		assert(!pos.m_ptrCur.is_null()); // pos must be valid
+		assert(pos.m_ptrCur != m_prehead); // pos must be valid
+
 		auto ptr = liw_new<AllocType, Element>(
-			Element{ liw_c_nullhdl, std::forward<const T>(val) }
+			Element{ liw_c_nullhdl, liw_c_nullhdl, std::forward<const T>(val) }
 		);
-		ptr->m_next = pos.m_ptrCur->m_next;
+		auto ptrNext = pos.m_ptrCur->m_next;
+
+		ptr->m_next = ptrNext;
+		ptrNext->m_prev = ptr;
 		pos.m_ptrCur->m_next = ptr;
+		ptr->m_prev = pos.m_ptrCur;
+
 		m_size++;
 		return Iterator(ptr);
 	}
 	Iterator insert_after(Iterator pos, T&& val) {
 		// Skipping check for if pos is in this container...
 		assert(!pos.m_ptrCur.is_null()); // pos must be valid
+
 		auto ptr = liw_new<AllocType, Element>(
-			Element{ liw_c_nullhdl, std::forward<T>(val) }
+			Element{ liw_c_nullhdl, liw_c_nullhdl, std::forward<T>(val) }
 		);
-		ptr->m_next = pos.m_ptrCur->m_next;
+		auto ptrNext = pos.m_ptrCur->m_next;
+
+		ptr->m_next = ptrNext;
+		ptrNext->m_prev = ptr;
 		pos.m_ptrCur->m_next = ptr;
+		ptr->m_prev = pos.m_ptrCur;
+
 		m_size++;
 		return Iterator(ptr);
 	}
@@ -290,22 +441,63 @@ public:
 		assert(!pos.m_ptrCur.is_null()); // pos must be valid
 		
 		auto ptrPrev = pos.m_ptrCur;
+		auto ptrNext = ptrPrev->m_next;
 		for (auto itr = beg; itr != end; ++itr) {
 			auto ptr = liw_new<AllocType, Element>(
-				Element{ liw_c_nullhdl, *itr }
+				Element{ liw_c_nullhdl, liw_c_nullhdl, *itr }
 			);
 			ptrPrev->m_next = ptr;
+			ptr->m_prev = ptrPrev;
 			ptrPrev = ptr;
 			m_size++;
 		}
+		ptrPrev->m_next = ptrNext;
+		ptrNext->m_prev = ptrPrev;
 		return Iterator(ptrPrev);
+	}
+
+	Iterator erase(Iterator pos) {
+		assert(!pos.m_ptrCur.is_null()); // pos must be valid
+		assert(pos.m_ptrCur != m_prehead); // pos must be valid
+
+		auto ptrPrev = pos.m_ptrCur->m_prev;
+		auto ptrNext = pos.m_ptrCur->m_next;
+
+		ptrNext->m_prev = ptrPrev;
+		ptrPrev->m_next = ptrNext;
+
+		liw_delete(pos.m_ptrCur);
+		m_size--;
+		return Iterator(ptrNext);
+	}
+	Iterator erase(Iterator beg, Iterator end) {
+		assert(!beg.m_ptrCur.is_null()); // beg must be valid
+		assert(beg.m_ptrCur != m_prehead); // beg must be valid
+
+		auto ptrEnd = end.m_ptrCur;
+		auto ptrPrev = beg.m_ptrCur->m_prev;
+		for (auto ptr = beg.m_ptrCur; ptr != ptrEnd; ) {
+			auto ptrNext = ptr->m_next;
+			liw_delete(ptr);
+			ptr = ptrNext;
+			m_size--;
+		}
+		ptrPrev->m_next = ptrEnd;
+		ptrEnd->m_prev = ptrPrev;
+		return end;
 	}
 
 	Iterator erase_after(Iterator pos) {
 		assert(!pos.m_ptrCur.is_null()); // pos must be valid
 		assert(!pos.m_ptrCur->m_next.is_null()); // next must be valid
+		assert(pos.m_ptrCur->m_next != m_prehead); // next must be valid
+
 		auto ptr = pos.m_ptrCur->m_next;
-		pos.m_ptrCur->m_next = ptr->m_next;
+		auto ptrNext = ptr->m_next;
+		
+		pos.m_ptrCur->m_next = ptrNext;
+		ptrNext->m_prev = pos.m_ptrCur;
+
 		liw_delete(ptr);
 		m_size--;
 		return Iterator(pos.m_ptrCur->m_next);
@@ -313,8 +505,9 @@ public:
 	Iterator erase_after(Iterator beg, Iterator end) {
 		assert(!beg.m_ptrCur.is_null()); // beg must be valid
 		assert(!beg.m_ptrCur->m_next.is_null()); // next must be valid
+		assert(beg.m_ptrCur->m_next != m_prehead); // next must be valid
 
-		const auto ptrEnd = end.m_ptrCur;
+		auto ptrEnd = end.m_ptrCur;
 		for (auto ptr = beg.m_ptrCur->m_next; ptr != ptrEnd; ) {
 			auto ptrNext = ptr->m_next;
 			liw_delete(ptr);
@@ -322,6 +515,7 @@ public:
 			m_size--;
 		}
 		beg.m_ptrCur->m_next = ptrEnd;
+		ptrEnd->m_prev = beg.m_ptrCur;
 		return end;
 	}
 
@@ -331,16 +525,17 @@ public:
 
 private:
 	inline void destroy_elements() {
-		while (!m_prehead->m_next.is_null())
+		while (m_prehead->m_next != m_prehead)
 		{
 			auto ptr = m_prehead->m_next;
 			m_prehead->m_next = ptr->m_next;
 			liw_delete(ptr);
 		}
+		m_prehead->m_prev = m_prehead;
 		m_size = 0;
 	}
 private:
 	size_t m_size{ 0 };
 	element_ptr_type m_prehead{ liw_c_nullhdl }; // Preceeding head. For easier operation.  
-												 // For any constructed LIWFowardList, m_prehead should never be null. 
+												 // For any constructed LIWList, m_prehead should never be null. 
 };
--- LIWEngine/test_container_0.h
+++ LIWEngine/test_container_0.h
@@ -5,7 +5,9 @@
 #include "Threading/LIWThread.h"
 #include "Utility/LIWFile.h"
 
+#include "Containers/LIWDArray.h"
 #include "Containers/LIWForwardList.h"
+#include "Containers/LIWList.h"
 
 
 using namespace LIW;
@@ -27,66 +29,214 @@ void test() {
 	liw_minit_dframe_thd(LIW_THREAD_IDX_MAIN);
 
 	{
-		LIWFowardList<int> list;
-		LIWFowardList<int> list1;
-		for (int i = 0; i < 10; ++i) {
-			list.push_front(i);
-			printf("%llu ", list.get_size());
+		{
+			LIWFowardList<int> list;
+			LIWFowardList<int> list1;
+			for (int i = 0; i < 10; ++i) {
+				list.push_front(i);
+				printf("%llu ", list.get_size());
+			}
+			printf("\n");
+			for (auto ptr = list.get_head(); !ptr.is_null(); ptr = ptr->m_next) {
+				printf("%d ", ptr->m_val);
+			}
+			printf("\n");
+
+			for (auto itr = list.get_beg(); itr != list.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			printf("\n");
+
+			list1.insert_after(list1.get_prebeg(), list.get_beg(), list.get_end());
+			for (auto ptr = list1.get_head(); !ptr.is_null(); ptr = ptr->m_next) {
+				printf("%d ", ptr->m_val);
+			}
+			printf("\n");
+			for (int i = 0; i < 4; ++i) {
+				list1.erase_after(list1.get_beg());
+			}
+			for (auto itr = list1.get_beg(); itr != list1.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			list1.erase_after(list1.get_beg(), list1.get_end());
+			for (auto itr = list1.get_beg(); itr != list1.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			printf("\n");
+
+			auto itr0 = list.get_beg();
+			itr0++;
+			itr0++;
+			itr0 = list.insert_after(itr0, 15);
+			itr0 = list.insert_after(itr0, 16);
+			itr0 = list.insert_after(itr0, 17);
+			for (auto itr = list.get_beg(); itr != list.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+
+
+
+
+			for (int i = 0; i < 10; ++i) {
+				list.pop_front();
+				printf("%llu ", list.get_size());
+			}
+			printf("\n");
+
+			list.clear();
+			printf("%llu\n", list.get_size());
+			printf("\n");
+			printf("\n");
+			printf("\n");
 		}
-		printf("\n");
-		for (auto ptr = list.get_head(); !ptr.is_null(); ptr = ptr->m_next) {
-			printf("%d ", ptr->m_val);
-		}
+		
+
+		printf("-------------------------\n");
 		printf("\n");
 
-		for (auto itr = list.get_beg(); itr != list.get_end(); itr++) {
-			printf("%d ", *itr);
-		}
-		printf("\n");
-		printf("\n");
-
-		list1.insert_after(list1.get_prebeg(), list.get_beg(), list.get_end());
-		for (auto ptr = list1.get_head(); !ptr.is_null(); ptr = ptr->m_next) {
-			printf("%d ", ptr->m_val);
-		}
-		printf("\n");
-		for (int i = 0; i < 4; ++i) {
-			list1.erase_after(list1.get_beg());
-		}
-		for (auto itr = list1.get_beg(); itr != list1.get_end(); itr++) {
-			printf("%d ", *itr);
-		}
-		printf("\n");
-		list1.erase_after(list1.get_beg(), list1.get_end());
-		for (auto itr = list1.get_beg(); itr != list1.get_end(); itr++) {
-			printf("%d ", *itr);
-		}
-		printf("\n");
-		printf("\n");
-
-		auto itr0 = list.get_beg();
-		itr0++;
-		itr0++;
-		itr0 = list.insert_after(itr0, 15);
-		itr0 = list.insert_after(itr0, 16);
-		itr0 = list.insert_after(itr0, 17);
-		for (auto itr = list.get_beg(); itr != list.get_end(); itr++) {
-			printf("%d ", *itr);
-		}
-		printf("\n");
+		//LIWDArray<int> a{20};
+		//for (int i = 0; i < 10; ++i) {
+		//	a.push_back(i);
+		//}
+		//auto ptr = a.get_ptr();
+		//for (int i = 0; i < 10; ++i) {
+		//	printf("%d ", *ptr);
+		//	ptr++;
+		//}
+		//printf("\n");
 
 
 
 
-		for (int i = 0; i < 10; ++i) {
-			list.pop_front();
-			printf("%llu ", list.get_size());
-		}
-		printf("\n");
+		{
+			LIWList<int> list;
+			LIWList<int> list1;
+			for (int i = 0; i < 10; ++i) {
+				list.push_front(i);
+				printf("%llu ", list.get_size());
+			}
+			printf("\n");
+			for (auto ptr = list.get_head(); ptr != list.get_prehead(); ptr = ptr->m_next) {
+				printf("%d ", ptr->m_val);
+			}
+			printf("\n");
 
-		list.clear();
-		printf("%llu\n", list.get_size());
-		printf("\n");
+			for (auto itr = list.get_beg(); itr != list.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			printf("\n");
+
+			list1.insert_after(list1.get_prebeg(), list.get_beg(), list.get_end());
+			for (auto ptr = list1.get_head(); ptr != list1.get_prehead(); ptr = ptr->m_next) {
+				printf("%d ", ptr->m_val);
+			}
+			printf("\n");
+			for (int i = 0; i < 4; ++i) {
+				list1.erase_after(list1.get_beg());
+			}
+			for (auto itr = list1.get_beg(); itr != list1.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			list1.erase_after(list1.get_beg(), list1.get_end());
+			for (auto itr = list1.get_beg(); itr != list1.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			printf("\n");
+
+			auto itr0 = list.get_beg();
+			itr0++;
+			itr0++;
+			itr0 = list.insert_after(itr0, 15);
+			itr0 = list.insert_after(itr0, 16);
+			itr0 = list.insert_after(itr0, 17);
+			for (auto itr = list.get_beg(); itr != list.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+
+
+
+
+			for (int i = 0; i < 4; ++i) {
+				int val = list.get_head_val();
+				list.pop_front();
+				printf("%d(%llu) ", val, list.get_size());
+			}
+			printf("\n");
+			for (int i = 0; i < 4; ++i) {
+				int val = list.get_tail_val();
+				list.pop_back();
+				printf("%d(%llu) ", val, list.get_size());
+			}
+			printf("\n");
+			for (auto itr = list.get_beg(); itr != list.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			printf("\n");
+			
+			auto itr1 = list1.get_beg();
+			for (int i = 0; i < 5; ++i) {
+				itr1 = list1.insert(itr1, 20 + i);
+			}
+			for (auto itr = list1.get_beg(); itr != list1.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			printf("\n");
+			
+			auto itr2 = list.get_beg();
+			itr2++;
+			itr2++;
+			itr2--;
+			itr2++;
+			printf("%d\n", *itr2);
+			itr2 = list.insert(itr2, list1.get_beg(), list1.get_end());
+			for (auto itr = list.get_beg(); itr != list.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			printf("%d\n", *itr2);
+			printf("\n");
+			printf("\n");
+
+			auto itr3 = itr2;
+			itr3--;
+			itr3++;
+			itr3++;
+			itr3++;
+			itr3++;
+			itr3 = list.erase(itr2, itr3);
+			for (auto itr = list.get_beg(); itr != list.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			printf("%d\n", *itr3);
+			list.erase(itr3);
+			for (auto itr = list.get_beg(); itr != list.get_end(); itr++) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			for (auto itr = --list.get_end(); itr != list.get_prehead(); itr--) {
+				printf("%d ", *itr);
+			}
+			printf("\n");
+			printf("\n");
+			
+
+			list.clear();
+			printf("%llu\n", list.get_size());
+			printf("\n");
+			printf("\n");
+		}
+		
 
 	}
 
